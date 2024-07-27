e255d8fd118c26325cac92fb5d11d575c878b5ca
DLAP <DLAP@DESKTOP-993BNM1>
2024-07-27 14:30:11 -0600

    Add project files.

---
+++ PokeGame-MVC/Controllers/SistemaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokeGame_MVC.Entities;
+
+namespace PokeGame_MVC.Controllers
+{
+    public class SistemaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Ingresar()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        //[Route("/Ingresar")]
+        public ActionResult Ingresar(UsuarioModel usuario) 
+        { 
+        
+
+            return View();  
+        }
+    }
+}
---
+++ PokeGame-MVC/Entities/UsuarioModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PokeGame_MVC.Entities
+{
+    public class UsuarioModel
+    {
+        public int RolId { get; set; }
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        public string PasswordHash { get; set; }
+
+        [Required(ErrorMessage = "El correo es requerido")]
+        public string Username { get; set; }
+        public string FotoPerfil { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+    }
+}
